cb58c844710f13f66897d1f95efaabcc43103698
akashbadola <Akashbadola3@gmail.com>
2024-07-30 23:54:16 +0530

    pushing the docker assignment files on github

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Hello! Welcome to my Web Page of G23AI2053'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
